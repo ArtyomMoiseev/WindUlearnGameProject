ada9acc8552522212e978bada61d51a0574fe5f9
ArtyomMoiseev <moiseev.artlenalex@gmail.com>
2021-04-12 22:50:27 +0500

    Add fan object.

---
+++ Wind/Wind.Models/FanMapObject.cs
@@ -0,0 +1,19 @@
+﻿namespace Wind.Models
+{
+    public class FanMapObject : IGameMapObject
+    {
+        public readonly double StreamForce;
+        public Direction Direction;
+        public readonly string ImageName;
+
+        public FanMapObject(double streamForce, Direction direction, string imageName)
+        {
+            StreamForce = streamForce;
+            Direction = direction;
+            ImageName = imageName;
+        }
+
+        public bool IsStaticObject => false;
+        public string ObjectImage => ImageName;
+    }
+}
