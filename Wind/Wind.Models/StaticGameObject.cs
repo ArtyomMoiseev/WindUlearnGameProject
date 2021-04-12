01780a1fc588b033d3835b44e9ec5377819f2362
ArtyomMoiseev <moiseev.artlenalex@gmail.com>
2021-04-12 22:26:10 +0500

    Describe static game object.

--- Wind/Wind.Models/StaticGameObject.cs
+++ Wind/Wind.Models/StaticGameObject.cs
@@ -2,7 +2,20 @@
 
 namespace Wind.Models
 {
-    public class StaticObject
+    public class StaticObject : IGameMapObject
     {
+        public readonly string ImageName;
+        public readonly double TurnAngle;
+        public readonly double Permeability;
+
+        public StaticObject(string imageName, double turnAngle, double permeability)
+        {
+            ImageName = imageName;
+            TurnAngle = turnAngle;
+            Permeability = permeability;
+        }
+
+        public bool IsStaticObject => true;
+        public string ObjectImage => ImageName;
     }
 }
